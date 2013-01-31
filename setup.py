d9095ca83e857a38bc80b8ea3489ba3529c86272
Juan Riaza <juanriaza@gmail.com>
2013-01-31 20:04:34 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('mega')
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': version}
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %(version)s -m 'version %(version)s'" % args
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name='python-mega',
+    version=version,
+    url='http://github.com/juanriaza/python-mega',
+    license='BSD',
+    description='Wrapper around the Mega API',
+    author='Juan Riaza',
+    author_email='juanriaza@gmail.com',
+    packages=get_packages('mega'),
+    package_data=get_package_data('mega'),
+    install_requires=open('requirements.txt').read().split('\n'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
---
+++ tests.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import unittest
+
+from mega import Mega
+
+
+class TestMega(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        email = os.environ.get('MEGAEMAIL') or sys.argv[0]
+        password = os.environ.get('MEGAPASSWORD') or sys.argv[1]
+        self.api_logged = Mega.from_credentials(email, password)
+        self.api_ephemeral = Mega.from_ephemeral()
+
+    def _check_file_exists(self, file_name, files):
+        uploaded = False
+        for f in files['f']:
+            if isinstance(f['a'], dict):
+                if f['a'].get('n') == file_name:
+                    uploaded = True
+        return uploaded
+
+    def _test_upload_file(self, api):
+        api.get_files()
+        api.uploadfile('tests.py') # inception
+        files = api.get_files()
+        uploaded = self._check_file_exists('tests.py', files)
+        self.assertEqual(uploaded, True)
+
+    def test_upload_file_logged(self):
+        self._test_upload_file(self.api_logged)
+
+    def test_upload_file_ephemeral(self):
+        self._test_upload_file(self.api_ephemeral)
+
+if __name__ == '__main__':
+    unittest.main()
